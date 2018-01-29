af9a94412068f606f28e8bc8bd468a3c994a9016
savitakohli <savita.kohli@gmail.com>
2018-01-28 22:59:36 -0500

    Update cachematrix.R
    1. New/ updated comments to explain the functions better.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,6 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## 1. Square matrix is passed as argument.
+## 2. setmatrix function calculates Inverse of matrix
+## 3. getmatrix function returns Inverse Matrix using function Solve.
 
 makeCacheMatrix <- function(x = matrix()) {
     m<-NULL
@@ -10,7 +9,9 @@ makeCacheMatrix <- function(x = matrix()) {
         m<<-NULL
     }
     get<-function() x
-    setmatrix<-function(solve) m<<- solve
+    ##setmatrix function calculates inverse of matrix
+    setmatrix<-function(solve) m<<- solve 
+    ##getmatrix function returns inverse matrix
     getmatrix<-function() m
     list(set=set, get=get,
          setmatrix=setmatrix,
@@ -18,8 +19,10 @@ makeCacheMatrix <- function(x = matrix()) {
     
 }
 
-
-## Write a short comment describing this function
+## 0. Function cacheSolve takes return value makeCacheMatrix (getmatrix) as argument, which is inverse of matrix passed as argument in makeCacheMatrix function.
+## 1. Functions looks for the Cached value of the getmatrix.  If it is not NULL, returns the cached value.
+## 2. If Cached value of the getmatrix is NULL, calculates  getmatrix and displays.
+## 3. If this function is run again with same matrix, displays the cached value.
 
 cacheSolve <- function(x, ...) {
     ## Return a matrix that is the inverse of 'x'
